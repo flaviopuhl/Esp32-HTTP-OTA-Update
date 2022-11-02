d4abdb667002c3552216bffb84fd20ab1b972bcb
Flávio Luiz Puhl Junior <33373207+flaviopuhl@users.noreply.github.com>
2022-11-02 11:46:29 -0300

    Create Esp32HttpOtaUpdate.cpp

---
+++ Esp32HttpOtaUpdate.cpp
@@ -0,0 +1,115 @@
+/**
+   httpUpdate.ino
+
+    Created on: 27.11.2015
+    Updated on: 02.11.2022
+
+*/
+
+#include <Arduino.h>
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <WiFiClientSecure.h>
+
+#include <HTTPClient.h>
+#include <HTTPUpdate.h>
+
+#define OTAFIRMWAREREPO "https://firebasestorage.googleapis.com/v0/b/firmwareota-a580e.appspot.com/o/ESP32OTAexample%2Ffirmware.bin?alt=media"
+
+
+WiFiMulti WiFiMulti;
+
+void setup() {
+
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+  //WiFiMulti.addAP("CasaDoTheodoro1", "09012011");
+  WiFi.begin("ssid", "password");
+
+  int wait_passes = 0;
+    while (WiFi.status() != WL_CONNECTED) {           // Wait for connection
+      delay(500);
+      Serial.print(".");
+      if (++wait_passes >= 20) { ESP.restart(); }     // Restart in case of no wifi connection   
+    }
+
+  Serial.print("\nWiFi connected");
+  Serial.print("\nIP address: ");
+    Serial.println(WiFi.localIP());
+
+}
+
+void update_started() {
+  Serial.println("CALLBACK:  HTTP update process started");
+}
+
+void update_finished() {
+  Serial.println("CALLBACK:  HTTP update process finished");
+}
+
+void update_progress(int cur, int total) {
+  Serial.printf("CALLBACK:  HTTP update process at %d of %d bytes...\n", cur, total);
+}
+
+void update_error(int err) {
+  Serial.printf("CALLBACK:  HTTP update fatal error code %d\n", err);
+}
+
+void loop() {
+
+  Serial.println("Version 1");
+  delay(1000);
+
+  // wait for WiFi connection
+  if ((WiFi.status() == WL_CONNECTED)) {
+
+    //WiFiClient client;
+    
+    WiFiClientSecure client; 
+    client.setInsecure();
+
+    // The line below is optional. It can be used to blink the LED on the board during flashing
+    // The LED will be on during download of one buffer of data from the network. The LED will
+    // be off during writing that buffer to flash
+    // On a good connection the LED should flash regularly. On a bad connection the LED will be
+    // on much longer than it will be off. Other pins than LED_BUILTIN may be used. The second
+    // value is used to put the LED on. If the LED is on with HIGH, that value should be passed
+    // httpUpdate.setLedPin(LED_BUILTIN, LOW);
+
+    httpUpdate.onStart(update_started);
+    httpUpdate.onEnd(update_finished);
+    httpUpdate.onProgress(update_progress);
+    httpUpdate.onError(update_error);
+
+    t_httpUpdate_return ret = httpUpdate.update(client, OTAFIRMWAREREPO);
+    // Or:
+    //t_httpUpdate_return ret = httpUpdate.update(client, "server", 80, "/file.bin");
+
+    switch (ret) {
+      case HTTP_UPDATE_FAILED:
+        Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+        break;
+
+      case HTTP_UPDATE_NO_UPDATES:
+        Serial.println("HTTP_UPDATE_NO_UPDATES");
+        break;
+
+      case HTTP_UPDATE_OK:
+        Serial.println("HTTP_UPDATE_OK");
+        break;
+    }
+  }
+}
